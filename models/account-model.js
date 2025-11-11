445f9ec801784e7bf74d74a26326fce88c627afa
divinemphame <mphametadiwanashe@gmail.com>
2025-11-11 15:52:24 +0200

    db rebuild

---
+++ models/account-model.js
@@ -0,0 +1,98 @@
+const pool = require("../database");
+
+/* *****************************
+ *   Register new account
+ * *************************** */
+async function registerAccount(
+  account_firstname,
+  account_lastname,
+  account_email,
+  account_password
+) {
+  try {
+    const sql =
+      "INSERT INTO account (account_firstname, account_lastname, account_email, account_password, account_type) VALUES ($1, $2, $3, $4, 'Client') RETURNING *";
+    return await pool.query(sql, [
+      account_firstname,
+      account_lastname,
+      account_email,
+      account_password,
+    ]);
+  } catch (error) {
+    return error.message;
+  }
+}
+
+/* ***********************************
+ * Check for existing email
+ * ********************************* */
+async function checkExistingEmail(account_email) {
+  try {
+    const sql = "SELECT * FROM account WHERE account_email = $1";
+    const email = await pool.query(sql, [account_email]);
+    return email.rowCount;
+  } catch (error) {
+    return error.message;
+  }
+}
+
+/* *****************************
+ * Return account data using email address
+ * ***************************** */
+async function getAccountByEmail(account_email) {
+  try {
+    const result = await pool.query(
+      "SELECT account_id, account_firstname, account_lastname, account_email, account_type, account_password FROM account WHERE account_email = $1",
+      [account_email]
+    );
+    return result.rows[0];
+  } catch (error) {
+    return new Error("No matching email found");
+  }
+}
+
+async function updateAccount(account_id, firstname, lastname, email) {
+  try {
+    const sql = `
+      UPDATE account
+      SET account_firstname = $1,
+          account_lastname = $2,
+          account_email = $3
+      WHERE account_id = $4
+      RETURNING *;
+    `;
+    const data = await pool.query(sql, [
+      firstname,
+      lastname,
+      email,
+      account_id,
+    ]);
+    return data.rowCount;
+  } catch (error) {
+    throw new Error("Database Error: " + error.message);
+  }
+}
+
+async function updatePassword(account_id, hashedPassword) {
+  try {
+    const sql = `
+      UPDATE account
+      SET account_password = $1
+      WHERE account_id = $2
+      RETURNING *;
+    `;
+    const data = await pool.query(sql, [hashedPassword, account_id]);
+    return data.rowCount;
+  } catch (error) {
+    console.error("Update password error:", error);
+    throw error;
+  }
+}
+
+module.exports = {
+  registerAccount,
+  checkExistingEmail,
+  getAccountByEmail,
+  updateAccount,
+  updatePassword,
+};
---
+++ models/inventory-model.js
@@ -0,0 +1,160 @@
+const pool = require("../database/");
+
+/* ***************************
+ *  Get all classification data
+ * ************************** */
+async function getClassifications() {
+  return await pool.query(
+    "SELECT * FROM public.classification ORDER BY classification_name"
+  );
+}
+
+/* ***************************
+ *  Get all inventory items and classification_name by classification_id
+ * ************************** */
+async function getInventoryByClassificationId(classification_id) {
+  try {
+    const data = await pool.query(
+      `SELECT * FROM public.inventory AS i 
+      JOIN public.classification AS c 
+      ON i.classification_id = c.classification_id 
+      WHERE i.classification_id = $1`,
+      [classification_id]
+    );
+    return data.rows;
+  } catch (error) {
+    console.error("getclassificationsbyid error " + error);
+  }
+}
+
+async function getVehicleById(inv_id) {
+  try {
+    const result = await pool.query(
+      "SELECT * FROM inventory WHERE inv_id = $1",
+      [inv_id]
+    );
+    return result.rows[0];
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function addClassification(classification_name) {
+  try {
+    const sql =
+      "INSERT INTO classification (classification_name) VALUES ($1) RETURNING  *";
+    const result = await pool.query(sql, [classification_name]);
+    return result.rows[0];
+  } catch (error) {
+    throw error;
+  }
+}
+
+/* ***************************
+ *  Add new inventory item
+ * ************************** */
+async function addInventoryItem(invData) {
+  try {
+    const sql = `
+      INSERT INTO inventory 
+      (inv_make, inv_model, inv_year, inv_description, inv_image, inv_thumbnail, inv_price, inv_miles, inv_color, classification_id) 
+      VALUES 
+      ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+      RETURNING *`;
+
+    const values = [
+      invData.inv_make,
+      invData.inv_model,
+      invData.inv_year,
+      invData.inv_description,
+      invData.inv_image,
+      invData.inv_thumbnail,
+      invData.inv_price,
+      invData.inv_miles,
+      invData.inv_color,
+      invData.classification_id,
+    ];
+
+    const result = await pool.query(sql, values);
+    return result.rows[0];
+  } catch (error) {
+    throw error;
+  }
+}
+
+/* ***************************
+ *  Update Inventory Data
+ * ************************** */
+async function updateInventory(
+  inv_id,
+  inv_make,
+  inv_model,
+  inv_description,
+  inv_image,
+  inv_thumbnail,
+  inv_price,
+  inv_year,
+  inv_miles,
+  inv_color,
+  classification_id
+) {
+  try {
+    const sql =
+      "UPDATE public.inventory SET inv_make = $1, inv_model = $2, inv_description = $3, inv_image = $4, inv_thumbnail = $5, inv_price = $6, inv_year = $7, inv_miles = $8, inv_color = $9, classification_id = $10 WHERE inv_id = $11 RETURNING *";
+    const data = await pool.query(sql, [
+      inv_make,
+      inv_model,
+      inv_description,
+      inv_image,
+      inv_thumbnail,
+      inv_price,
+      inv_year,
+      inv_miles,
+      inv_color,
+      classification_id,
+      inv_id,
+    ]);
+    return data.rows[0];
+  } catch (error) {
+    console.error("model error: " + error);
+  }
+}
+
+/* ***************************
+ *  Delete Inventory Item
+ * ************************** */
+async function deleteInventoryItem(inv_id) {
+  try {
+    const sql = "DELETE FROM inventory WHERE inv_id = $1";
+    const data = await pool.query(sql, [inv_id]);
+    return data;
+  } catch (error) {
+    throw new Error("Delete Inventory Error");
+  }
+}
+
+/* ***************************
+ *  Wk06 - Enhacement Idea
+ * ************************** */
+
+async function updateVehicleStatus(inv_id, inv_status) {
+  const sql = `
+    UPDATE inventory
+    SET inv_status = $1
+    WHERE inv_id = $2
+    RETURNING *;
+  `;
+  return await pool.query(sql, [inv_status, inv_id]);
+}
+
+
+module.exports = {
+  getClassifications,
+  getInventoryByClassificationId,
+  getVehicleById,
+  addClassification,
+  addInventoryItem,
+  updateInventory,
+  deleteInventoryItem,
+  updateVehicleStatus
+};
